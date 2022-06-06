fbd1aff0b2166c7cc7a08de071785111d88233cf
uijun lee <lejpower@naver.com>
2022-06-06 13:04:42 +0900

    edit some bugs

--- for_mysql57_reboot.sh
+++ for_mysql57_reboot.sh
@@ -42,13 +42,13 @@ EOF
 fi
 
 #############
-# set args 
+# set args
 #############
 
 ARGS=$#
 
 set_opt()
-{	
+{
   if [ "$1" = "-h" ]; then
     HOST="$2"
   elif [ "$1" = "-u" ]; then
@@ -90,33 +90,41 @@ MYSQL_CMD_INFORMATION_SCHEMA="mysql -N information_schema -h${HOST} -u${USER} -p
 MYSQL_CMD_VERBOSE="mysql -vvv ${DB} -h${HOST} -u${USER} -p${PASSWORD} -P${PORT}"
 MYSQL_CMD_VERBOSE2="mysql -vv ${DB} -h${HOST} -u${USER} -p${PASSWORD} -P${PORT}"
 
+######################
+# make log directory
+######################
+LOG_DIR="./LOG/${DB}_`date '+%Y%m%d'`"
+mkdir -p ${LOG_DIR}
+
+
 ############
 # check schema
 ############
 
 chk_schema()
 {
-  if [ -e ${LOG_PREFIX}_auto_inc.log ]; then
-    \rm -if ${LOG_PREFIX}_auto_inc.log
-  fi	
+  if [ -e ${LOG_DIR}/${LOG_PREFIX}_auto_inc.log ]; then
+    \rm -if ${LOG_DIR}/${LOG_PREFIX}_auto_inc.log
+  fi
   if [ ${LOG_PREFIX} = "before" ]; then
-    CREATE_TABLE_LIST=`echo "show tables;" | ${MYSQL_CMD} | sort > table.list`
+    CREATE_TABLE_LIST=`echo "show tables;" | ${MYSQL_CMD} | sort > ${LOG_DIR}/table.list`
   fi
   echo "[Get Auto_increment values]"
   while read LINE
   do
     AUTO_INC_CNT=`echo "select AUTO_INCREMENT from TABLES where TABLE_NAME = '${LINE}';" | ${MYSQL_CMD_INFORMATION_SCHEMA} | grep -v "AUTO_INCREMENT"`
     #AUTO_INC_CNT=`echo "show create table ${LINE} \G" | ${MYSQL_CMD} | grep "ENGINE=" | awk '{print $3}' | cut -d "=" -f 2`
-    echo "${LINE} ${AUTO_INC_CNT}" | tee -a ${LOG_PREFIX}_auto_inc.log
-  done < table.list
-  echo "############" 
-  echo "############" 
+    echo "${LINE} ${AUTO_INC_CNT}" | tee -a ${LOG_DIR}/${LOG_PREFIX}_auto_inc.log
+    sleep 0.1
+  done < ${LOG_DIR}/table.list
+  echo "############"
+  echo "############"
   echo ""
 }
 
 
 ##########################
-# sync auto_increment 
+# sync auto_increment
 ##########################
 
 sync_auto_inc()
@@ -124,27 +132,30 @@ sync_auto_inc()
   echo "[Fix Auto_increment values]"
   while read TABLE_NAME
   do
-     echo "-----" | tee -a operation.log
-     bf_cnt=`grep "${TABLE_NAME}" before_auto_inc.log | awk '{print $2}'`
-     af_cnt=`grep "${TABLE_NAME}" after_auto_inc.log | awk '{print $2}'`
-     echo ${TABLE_NAME} | tee -a operation.log
-     echo "before : ${bf_cnt}" | tee -a operation.log
-     echo "after  : ${af_cnt}" | tee -a operation.log
-      
-     if [ ${bf_cnt} -gt ${af_cnt} ]; then 
-       echo "-> Before is greater than After : ${bf_cnt}"  | tee -a operation.log
+     echo "-----" | tee -a ${LOG_DIR}/operation.log
+     bf_cnt=`grep -w ${TABLE_NAME} ${LOG_DIR}/before_auto_inc.log | awk '{print $2}'`
+     af_cnt=`grep -w ${TABLE_NAME} ${LOG_DIR}/after_auto_inc.log | awk '{print $2}'`
+     echo ${TABLE_NAME} | tee -a ${LOG_DIR}/operation.log
+     echo "before : ${bf_cnt}" | tee -a ${LOG_DIR}/operation.log
+     echo "after  : ${af_cnt}" | tee -a ${LOG_DIR}/operation.log
+
+     if [ ${bf_cnt} = "NULL" ]; then
+       echo "-> Skipped because of NULL number" | tee -a ${LOG_DIR}/operation.log
+     elif [ ${bf_cnt} -gt ${af_cnt} ]; then
+       echo "-> Before is greater than After : ${bf_cnt}"  | tee -a ${LOG_DIR}/operation.log
        echo ""
-       echo "ALTER TABLE ${TABLE_NAME} AUTO_INCREMENT = ${bf_cnt} ;" | ${MYSQL_CMD_VERBOSE} | tee -a operation.log
-     elif [ ${bf_cnt} -lt ${af_cnt} ]; then  
-       echo "-> Skipped ALTER operation because After is greater than Before : ${af_cnt}"  | tee -a operation.log
-     elif [ ${bf_cnt} -eq ${af_cnt} ]; then  
-       echo "-> Skipped because of the Same number!" | tee -a operation.log
+       echo "ALTER TABLE ${TABLE_NAME} AUTO_INCREMENT = ${bf_cnt} ;" | ${MYSQL_CMD_VERBOSE} | tee -a ${LOG_DIR}/operation.log
+       sleep 0.1
+     elif [ ${bf_cnt} -lt ${af_cnt} ]; then
+       echo "-> Skipped ALTER operation because After is greater than Before : ${af_cnt}"  | tee -a ${LOG_DIR}/operation.log
+     elif [ ${bf_cnt} -eq ${af_cnt} ]; then
+       echo "-> Skipped because of the Same number!" | tee -a ${LOG_DIR}/operation.log
      fi
-     echo " " | tee -a operation.log
-  done < table.list
+     echo " " | tee -a ${LOG_DIR}/operation.log
+  done < ${LOG_DIR}/table.list
   echo ""
-  echo "############" 
-  echo "############" 
+  echo "############"
+  echo "############"
   echo ""
 }
 
@@ -154,16 +165,16 @@ sync_auto_inc()
 
 disable_DB_permission()
 {
-  if [ -e disable_permission.log ]; then
-    \rm -if disable_permission.log
+  if [ -e ${LOG_DIR}/disable_permission.log ]; then
+    \rm -if ${LOG_DIR}/disable_permission.log
   fi
   echo "[Permission Disable]"
-  echo "update mysql.db set Db='tmp_${DB}' where Db='${DB}'; flush privileges;" | ${MYSQL_CMD_VERBOSE2} | tee -a disable_permission.log 
-  echo " " | tee -a disable_permission.log
-  echo "############" | tee -a disable_permission.log
-  echo "############" | tee -a disable_permission.log
-  echo " " | tee -a disable_permission.log
-  echo "select Host,Db,User from mysql.db;" | ${MYSQL_CMD_VERBOSE} | tee -a disable_permission.log
+  echo "update mysql.db set Db='tmp_${DB}' where Db='${DB}'; flush privileges;" | ${MYSQL_CMD_VERBOSE2} | tee -a ${LOG_DIR}/disable_permission.log
+  echo " " | tee -a ${LOG_DIR}/disable_permission.log
+  echo "############" | tee -a ${LOG_DIR}/disable_permission.log
+  echo "############" | tee -a ${LOG_DIR}/disable_permission.log
+  echo " " | tee -a ${LOG_DIR}/disable_permission.log
+  echo "select Host,Db,User from mysql.db;" | ${MYSQL_CMD_VERBOSE} | tee -a ${LOG_DIR}/disable_permission.log
   echo ""
   echo ""
 }
@@ -174,20 +185,20 @@ disable_DB_permission()
 
 enable_DB_permission()
 {
-  if [ -e enable_permission.log ]; then
-    \rm -if enable_permission.log
+  if [ -e ${LOG_DIR}/enable_permission.log ]; then
+    \rm -if ${LOG_DIR}/enable_permission.log
   fi
   echo "[Permission Enable]"
-  echo "update mysql.db set Db='${DB}' where Db='tmp_${DB}'; flush privileges;" | ${MYSQL_CMD_VERBOSE2} | tee -a enable_permission.log
-  echo " " | tee -a enable_permission.log
-  echo "############" | tee -a enable_permission.log
-  echo "############" | tee -a enable_permission.log
-  echo " " | tee -a enable_permission.log
-  echo "select Host,Db,User from mysql.db;" | ${MYSQL_CMD_VERBOSE} | tee -a enable_permission.log
+  echo "update mysql.db set Db='${DB}' where Db='tmp_${DB}'; flush privileges;" | ${MYSQL_CMD_VERBOSE2} | tee -a ${LOG_DIR}/enable_permission.log
+  echo " " | tee -a ${LOG_DIR}/enable_permission.log
+  echo "############" | tee -a ${LOG_DIR}/enable_permission.log
+  echo "############" | tee -a ${LOG_DIR}/enable_permission.log
+  echo " " | tee -a ${LOG_DIR}/enable_permission.log
+  echo "select Host,Db,User from mysql.db;" | ${MYSQL_CMD_VERBOSE} | tee -a ${LOG_DIR}/enable_permission.log
   echo ""
   echo ""
   echo "[diff]"
-  diff -s disable_permission.log enable_permission.log | tee diff.log
+  diff -s ${LOG_DIR}/disable_permission.log ${LOG_DIR}/enable_permission.log | tee ${LOG_DIR}/diff.log
 }
 
 
@@ -199,16 +210,16 @@ enable_DB_permission()
 case ${OPT} in
   'bf')
         LOG_PREFIX="before"
-        if [ -e table.list ]; then
-          \rm -if table.list
+        if [ -e ${LOG_DIR}/table.list ]; then
+          \rm -if ${LOG_DIR}/table.list
         fi
-        chk_schema
         disable_DB_permission
+        chk_schema
         ;;
   'af')
         LOG_PREFIX="after"
-        if [ -e operation.log ]; then
-          \rm -if operation.log
+        if [ -e ${LOG_DIR}/operation.log ]; then
+          \rm -if ${LOG_DIR}/operation.log
         fi
         chk_schema
         sync_auto_inc
